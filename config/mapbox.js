4f730a79fcfed4502e5c9a94d76c117e16f7864a
Guro Prestegard <gurop@stacc.com>
2022-08-23 11:30:44 +0200

    init mapbox

---
+++ config/mapbox.js
@@ -0,0 +1,6 @@
+import ReactMapboxGl from "react-mapbox-gl";
+
+export default ReactMapboxGl({
+    accessToken:
+        process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN
+});
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+        NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN,
+    },
 }
 
 module.exports = nextConfig
