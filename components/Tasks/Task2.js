d74f057a6b3cdb72d2ad5202e1a0f230f9b490a8
Rein Undheim <rein.undheim@protonmail.com>
2022-08-25 21:07:13 +0200

    feat: adds temp text to task 2

--- components/Tasks/Task2.js
+++ components/Tasks/Task2.js
@@ -1,7 +1,27 @@
 export default function Task2() {
   return (
     <div>
-      <p>Task2</p>
+      <p>
+        <b>Oppgave: </b>Endre bakgrunnsfarge (eller hva du vil på siden, the sky
+        is the limit) på "Header" komponenten.
+      </p>
+      <br />
+      <p>
+        Nå som vi kan opprette sider og hente data til disse, bør vi faktisk få
+        sidene våre til å se bra ut! Vi skal bruke et språk som heter "CSS" til
+        å gjøre dette. Vi skriver css i filer som slutter på ".module.css", og
+        du kan se noen eksempler på dette allerede.
+      </p>
+      <br />
+      <p>
+        Først og fremst ønsker vi å endre på utseendet til "Header" komponenten,
+        men du spør kanskje deg selv, "hva er en komponent?". Komponenter er en
+        måte å bryte opp kode i flere mindre deler. Vi kan da ta ut kode og
+        definere det i separate filer, og senere kalle på disse filene og dermed
+        ha mindre kodelinjer i hovedsiden. Komponenter i NextJs blir lagret i en
+        mappe som heter "components". Navigerer til "Header" komponenten og se
+        om du får til å endre på fargen!
+      </p>
     </div>
   );
 }
