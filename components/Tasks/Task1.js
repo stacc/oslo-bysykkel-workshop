1b0d6e29a5ad092960feb8863124b9bf8eb2333a
Rein Undheim <rein.undheim@protonmail.com>
2022-08-29 21:00:22 +0200

    chore: styling, change banner and delete documentation page

--- components/Tasks/Task1.js
+++ components/Tasks/Task1.js
@@ -15,16 +15,17 @@ export default function Task1() {
       <p>
         NextJS er et rammeverk som bygger på React, og som gir oss litt enklere
         måter å navigere rundt på siden med Linker og måter å hente data på! Et
-        av de viktigste konseptene i NextJS er <em>pages</em> mappen. Alle filer
-        i<em> pages</em> mappen vil bli en ny side på nettsiden vår. For
-        eksempel hvis en lager en fil i <em>pages</em> som heter <em>banan</em>{" "}
-        vil url-en til denne siden bli <em>localhost:3000/banan</em>. Det er
-        også mulig å bruke mapper til å ha filer i, disse vil ikke generere en
-        egen side selv, men hvis en har en fil inne i mappen, så vil navnet på
-        mappen bli lagt til i url-en. Hvis vi tar bananan eksempelet vårt, og vi
-        putter
-        <em> banan.js</em> inne i en mappe som heter <em>frukter</em>, så vil
-        url-en til denne siden bli <em>localhost:3000/frukter/banan</em>.
+        av de viktigste konseptene i NextJS er <code>pages</code> mappen. Alle
+        filer i<code> pages</code> mappen vil bli en ny side på nettsiden vår.
+        For ekscodepel hvis en lager en fil i <code>pages</code> som heter{" "}
+        <code>banan</code> vil url-en til denne siden bli{" "}
+        <code>localhost:3000/banan</code>. Det er også mulig å bruke mapper til
+        å ha filer i, disse vil ikke generere en egen side selv, men hvis en har
+        en fil inne i mappen, så vil navnet på mappen bli lagt til i url-en.
+        Hvis vi tar bananan ekscodepelet vårt, og vi putter
+        <code> banan.js</code> inne i en mappe som heter <code>frukter</code>,
+        så vil url-en til denne siden bli{" "}
+        <code>localhost:3000/frukter/banan</code>.
       </p>
 
       <Image
@@ -37,9 +38,10 @@ export default function Task1() {
       <p>
         Datahenting i NextJS foregår ofte via å bruke funksjoner som puttes på
         bunnen av sidene våres, og vil i disse oppgavene være en funksjon som
-        heter <em>getStaticProps</em>. Funksjonene sender data til siden vår som
-        <em> props</em>. Dette er lettere å se i praksis, så se i koden på en av
-        de allerede eksisterende sidene.
+        heter <code>getStaticProps</code>. Funksjonene sender data til siden vår
+        som
+        <code> props</code>. Dette er lettere å se i praksis, så se i koden på
+        en av de allerede eksisterende sidene.
       </p>
 
       <Image
--- components/Tasks/Task2.js
+++ components/Tasks/Task2.js
@@ -8,17 +8,18 @@ export default function Task2() {
       <TLDR>
         <p>
           <b>Kort fortalt: </b>Endre bakgrunnsfarge (eller hva du vil på siden,
-          the sky is the limit) på <em>Header</em> komponenten. Se om du også
-          skjønner hvordan stilsettingen fungerer.
+          the sky is the limit) på <code>Header</code> komponenten. Se om du
+          også skjønner hvordan stilsettingen fungerer.
         </p>
       </TLDR>
       <br />
       <p>
         Nå som vi kan opprette sider og hente data til disse, bør vi faktisk få
         sidene våre til å se bra ut! Vi skal bruke et språk som heter{" "}
-        <em>CSS</em> til å gjøre dette. Vi skriver css i filer som slutter på
-        <em> .module.css</em>, og du kan se noen eksempler på dette allerede i
-        filene.
+        <code>CSS</code> til å gjøre dette. Vi skriver css i filer som slutter
+        på
+        <code> .module.css</code>, og du kan se noen ekscodepler på dette
+        allerede i filene.
       </p>
       <Image
         src="/images/components.png"
@@ -27,13 +28,13 @@ export default function Task2() {
         className={styles.image}
       />
       <p>
-        Først og fremst ønsker vi å endre på utseendet til <em>Header</em>{" "}
+        Først og fremst ønsker vi å endre på utseendet til <code>Header</code>{" "}
         komponenten, men du spør kanskje deg selv, "hva er en komponent?".
         Komponenter er en måte å bryte opp kode i flere mindre deler. Vi kan da
         ta ut kode og definere det i separate filer, og senere kalle på disse
         filene og dermed ha mindre kodelinjer i hovedsiden. Komponenter i NextJs
-        blir lagret i en mappe som heter <em>components</em>. Navigerer til
-        <em> Header</em> komponenten og se om du får til å endre på fargen!
+        blir lagret i en mappe som heter <code>components</code>. Navigerer til
+        <code> Header</code> komponenten og se om du får til å endre på fargen!
       </p>
     </div>
   );
