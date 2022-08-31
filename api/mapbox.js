d07d3546bcf0f40835b045e10fd3e5e40d2e44b3
Rein Undheim <rein.undheim@protonmail.com>
2022-08-31 21:46:45 +0200

    feat: flesh out task 5

--- api/mapbox.js
+++ api/mapbox.js
@@ -1,19 +1,38 @@
-import axios from "axios"
+import axios from "axios";
 
 const mapboxAxios = axios.create({
-    baseURL: "https://api.mapbox.com",
-    params: {
-        access_token: process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN
-    }
-})
+  baseURL: "https://api.mapbox.com",
+  params: {
+    access_token: process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN,
+  },
+});
 
-export const getCycleRoute = async (coordinates, options={format: "geojson"}) => {
-    const points = coordinates.reduce((route, coord) => {
-        return [route, `${coord.long},${coord.lat}`].filter(x=>!!x).join(";")
-    }, "")
+export const getCycleRoute = async (
+  coordinates,
+  options = { format: "geojson" }
+) => {
+  const points = coordinates.reduce((route, coord) => {
+    return [route, `${coord.long},${coord.lat}`].filter((x) => !!x).join(";");
+  }, "");
 
-    const { data } = await  mapboxAxios.get(`/directions/v5/mapbox/cycling/${points}?continue_straight=true&geometries=${options.format}&overview=simplified`, {
-    })
-    return data
-}
+  const { data } = await mapboxAxios.get(
+    `/directions/v5/mapbox/cycling/${points}?continue_straight=true&geometries=${options.format}&overview=simplified`,
+    {}
+  );
+  return data;
+};
 
+export const getPedestrianRoute = async (
+  coordinates,
+  options = { format: "geojson" }
+) => {
+  const points = coordinates.reduce((route, coord) => {
+    return [route, `${coord.lon},${coord.lat}`].filter((x) => !!x).join(";");
+  }, "");
+
+  const { data } = await mapboxAxios.get(
+    `/directions/v5/mapbox/walking/${points}?continue_straight=true&geometries=${options.format}&overview=simplified`,
+    {}
+  );
+  return data;
+};
