1873ff3c13dd5746031e2227f0cfb8f352a79360
Guro Prestegard <gurop@stacc.com>
2022-09-08 16:32:46 +0200

    fix: update text

--- components/Tasks/Task4.js
+++ components/Tasks/Task4.js
@@ -6,7 +6,6 @@ import {Layer, Source} from "react-map-gl";
 import React, {useState} from "react";
 import {getCycleRoute} from "../../api/mapbox";
 import {formatStationsForDropdown} from "../../utils/formatStationsForDropdown";
-import DropDown from "../InputFields/DropDown";
 
 const Task4 = ({stations}) => {
     const choices = formatStationsForDropdown(stations)
@@ -42,38 +41,71 @@ const Task4 = ({stations}) => {
             <TLDR>
                 <p>
                     <b>Kort fortalt: </b>
-                    Bruk elementene du finner i <code>/components/InputFields</code> for å lage et skjema,
-                    slik at du kan velge avreise- og ankomststativer, og vis ruten på kartet.
+                    Bruk dropdown-elementene du finner i <code>/components/InputFields</code> for å fullføre skjemaet i
+                    bunnen,
+                    slik at du kan velge avreise- og ankomststativer, og vise ruten på kartet.
                     Du må prosessere dataene returnert fra skjemaet slik at det passer med formatet kartet trenger.
                 </p>
             </TLDR>
             <p className={styles.section}>
-                I oppgave 3 og oppgave 5, jobbet vi med på vise ruten mellom to ulike stativene, og å lage en
-                dropdown-liste
-                med alle de tilgjengelige stativene. I denne oppgaven skal vi koble disse elementene sammen - slik at du
+                I forrige oppgave jobbet vi med å lage en dropdown-liste med alle de tilgjengelige stativene. I denne
+                oppgaven skal vi videre med dette elementet - slik at du
                 kan velge stasjoner fra to lister, og vise ruta mellom de på kartet.
             </p>
             <p className={styles.section}>
                 For å vise ruta på kartet, må vi vite hvor vi skal fra, og hvor vi skal til. Vi
-                bruker <code>geoJson</code>
-                og <code>getCycleRoute</code> som vi har brukt tidligere, og henter input fra bruker gjennom
+                bruker <code>geoJson</code> (et dataformat som viser enkle geografiske strukturer)
+                og <code>getCycleRoute</code> (en funksjon vi har skrevet som bruker et endepunkt fra mapBox for å
+                kalkulerer sykkelruter), og henter input fra bruker gjennom
                 dropdown-komponenter.
             </p>
-            <ol className={styles.section}>
+            <b>Å bruke komponenter</b>
+            <p className={styles.section}>
+                I React bruker vi <em>komponenter</em>, som er biter med kode som kan gjenbrukes.
+                <br/>
+                <br/>
+                For å bruke komponenter, må de først importeres. Det ser vanligvis slik ut:
+                <br/>
+                <code>import Apple from "../../fruits";</code>
+                Når komponentet er importert, kan det brukes i return-funksjonen:
+                <br/>
+                <br/>
+                <code>return(
+                    <br/>
+                    &lt;Apple/&gt;
+                    <br/>
+                    )</code>
+                <br/>
+                <br/>
+                Et komponent kan også ta inn data, på samme måte som en funksjon. Man kaller
+                dette <em>props</em>. Om  &lt;Apple/&gt;-komponentet for eksempel tar inn props for vekt og farge,
+                ville det sett slik ut:
+                <br/>
+                <br/>
+                <code>return(
+                    <br/>
+                    &lt;Apple weight=&#123;0.5&#125; color=&#123;"red"&#125;/&gt;
+                    <br/>
+                    )</code>
+
+
+
+            </p>
+            <b>Oppgaver</b>
+            <ul className={styles.listWithIcons}>
                 <li>
-                    Åpne <code>components/tasks/Task6.js</code>
+                    Åpne <code>components/tasks/Task4.js</code>
                 </li>
                 <li>
                     Importer <code>DropDown.js</code> og legg til to dropdownlister i <code>form</code>-elementet under.
                     Den ene skal representere <em>avreisestativer</em>,
                     det andre skal representere <em>ankomststativer</em>. Du finner dropdownkomponentet
-                    i <code>/components/inputFields/</code>. Kikk på hva dropdown-komponentet
+                    i <code>/components/inputFields/</code>. Se på hva dropdown-komponentet
                     trenger for å fungere, og pass på å få det med som props.
                 </li>
                 <li>
-                    Som du ser, så har form-komponentet en funksjon som kjører på <code>onSubmit</code>,
-                    nemlig <code>onSubmit</code>.
-                    Denne funksjonen får inn resultatet fra formet, når den blir trigget gjennom "Finn reise"-knappen.
+                    Når "Finn reise"-knappen trykkes, blir <code>onSubmit</code>-funksjonen trigget.
+                    Denne funksjonen får inn resultatet fra formet.
                     Få <em>console.log()</em>-funksjonen på linje 24 til å logge ut avreise- og ankomststativene.
                 </li>
                 <li>
@@ -81,7 +113,11 @@ const Task4 = ({stations}) => {
                     fyll dem inn i <em>trip</em>-lista. Her trenger vi to objekter; et for avreise, og et for ankomst.
                     Objektene skal ha to <em>key-value</em>-par, med key: <code>long</code> og <code>lat</code>.
                 </li>
-            </ol>
+                <li>
+                   Har du gjort alt rett, vil du nå se et kart med en sykkelrute under! 🎉
+                </li>
+
+            </ul>
 
             <p className={styles.section}>Hvor skal vi reise?</p>
             <form className={styles.form} id={"routePlanner"} onSubmit={onSubmit}>
